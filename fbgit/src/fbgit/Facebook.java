bd618b2044d9c6bd5c12319f2252c41da1d46796
SHIJIJTHA <fathimashahabudeen95@gmail.com>
2024-05-13 16:13:44 +0530

    commit

--- fbgit/src/fbgit/Facebook.java
+++ fbgit/src/fbgit/Facebook.java
@@ -10,7 +10,9 @@ public class Facebook {
 		
 		System.setProperty("webdriver.gecko.driver","C:\\Users\\DELL\\Downloads\\geckodriver-v0.26.0-win64\\geckodriver.exe");
 		WebDriver driver=new FirefoxDriver();
-		driver.get("https://www.facebook.com");
+		System.outprintln("hello");
+			
+				driver.get("https://www.facebook.com");
 		driver.findElement(By.name("email")).sendKeys("praveena");
 		driver.findElement(By.name("pass")).sendKeys("pravee");
 		driver.findElement(By.name("login")).click();
